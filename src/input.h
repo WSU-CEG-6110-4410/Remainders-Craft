1ce5a63b0fea8a1711b29b0ccb947b48802aad2c
Erik M. Buck <erik.buck@sbcglobal.net>
2022-04-07 14:47:41 -0400

    Put some functions back into main.c

--- src/input.h
+++ src/input.h
@@ -3,14 +3,6 @@
 #ifndef _input_h_
 #define _input_h_
 
-void get_motion_vector(int flying, int sz, int sx, float rx, float ry, float *vx, float *vy, float *vz);
-
-int collide(int height, float *x, float *y, float *z, Model *model);
-
-int highest_block(float x, float z, Model *model);
-
-void handle_movement(double dt, Model *model);
-
 void handle_mouse_input(Model *model);
 
 void get_sight_vector(float rx, float ry, float *vx, float *vy, float *vz);
