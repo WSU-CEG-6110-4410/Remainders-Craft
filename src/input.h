7de5d6a3bc117bf68264a7d914f77e7bedc8cf99
Erik M. Buck <erik.buck@sbcglobal.net>
2022-04-13 11:40:24 -0400

    Document the first click functions

--- src/input.h
+++ src/input.h
@@ -4,14 +4,32 @@
 #include "structs.h"
 
 /// [issue](https://github.com/WSU-CEG-6110-4410/Remainders-Craft/issues/8)
-/// These functions were derived from main.c. Further documentation is necessary.
+/// [issue](https://github.com/WSU-CEG-6110-4410/Remainders-Craft/issues/10)
 
+/// Use this function to translate cursor movements
+/// for a given framerate of the game instance.
+///\param[in,out] model: The current game instance.
+/// The player state will be translated according
+/// to the cursor position.
 void handle_mouse_input(Model *model);
 
+/// Use this function to produce an action for
+/// a left click. It attempts to set a block.
+///\param[in,out]: The current game instance.
+/// Might be modified if a block can be set.
 void on_left_click(Model *model);
 
+/// Use this function to produce an action for
+/// a right click. It attempts to destroy a block.
+///\param[in,out]: The current game instance.
+/// Might be modified if a block can be destroyed.
 void on_right_click(Model *model);
 
+/// Use this function to produce an action for
+/// a middle click. It cycles through the block which
+/// the user can set.
+///\param[in,out]: The current game instance.
+/// Will be modified to change item_index.
 void on_middle_click(Model *model);
 
 void toggle_light(int x, int y, int z, Model *model);
